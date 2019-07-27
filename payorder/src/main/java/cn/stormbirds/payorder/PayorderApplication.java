4ebad7e1b086df5548167ee658739cde0e9e3940
stormbirds <xbaojun@gmail.com>
2019-07-27 19:28:48 +0800

    Initial commit

---
+++ payorder/src/main/java/cn/stormbirds/payorder/PayorderApplication.java
@@ -0,0 +1,13 @@
+package cn.stormbirds.payorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayorderApplication.class, args);
+    }
+
+}
---
+++ payorder/src/test/java/cn/stormbirds/payorder/PayorderApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.stormbirds.payorder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PayorderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
